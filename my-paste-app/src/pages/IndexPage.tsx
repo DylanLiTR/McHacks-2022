23c5d85b518fb6012e0ca79c60357811fca4b1b6
Dylan Li <dylanli0629@gmail.com>
2022-01-22 16:07:44 -0500

    Twilio Update

--- my-paste-app/src/pages/IndexPage.tsx
+++ my-paste-app/src/pages/IndexPage.tsx
@@ -5,6 +5,7 @@ import {Anchor} from '@twilio-paste/core/anchor';
 import {Button} from '@twilio-paste/core/button';
 import {Heading} from '@twilio-paste/core/heading';
 import {PlusIcon} from '@twilio-paste/icons/esm/PlusIcon';
+import {Flex} from '@twilio-paste/core/flex';
 
 interface FancyBoxProps {
   padding: BoxStyleProps['padding'];
@@ -15,18 +16,51 @@ const FancyBox: React.FC<FancyBoxProps> = styled(Box)({
 
 export const IndexPage: React.FC = () => {
   return (
-    <Box margin="space60">
-      <Heading as="h1" variant="heading10">
-        Start here.
-      </Heading>
-      <Button variant="primary">
-        <PlusIcon decorative />
-        Primary button
-      </Button>
-      <FancyBox padding="space90">Fancy box</FancyBox>
-      <Anchor href="https://paste.twilio.design" showExternal>
-        Paste Design System
-      </Anchor>
+    <Box>
+      <Flex>
+        <Flex>
+          <Box
+            backgroundColor="colorBackgroundPrimaryWeak"
+            padding="space40"
+            height="100vh"
+            width="size60"
+          >
+          </Box>
+        </Flex>
+        <Flex grow>
+          <Box
+            backgroundColor="colorBackgroundPrimaryWeaker"
+            padding="space40"
+            width="100%"
+            height="100vh"
+          >
+            <Heading as="h1" variant="heading10">
+              पानीpeers
+            </Heading>
+          </Box>
+        </Flex>
+        <Flex>
+          <Box
+            backgroundColor="colorBackgroundPrimaryWeak"
+            padding="space40"
+            height="100vh"
+            width="size60"
+          >
+            <Button variant="primary">
+              <PlusIcon decorative />
+              100mL
+            </Button><br></br>
+            <Button variant="primary">
+              <PlusIcon decorative />
+              250mL
+            </Button><br></br>
+            <Button variant="primary">
+              <PlusIcon decorative />
+              Custom
+            </Button>
+          </Box>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
