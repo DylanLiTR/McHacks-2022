2539c9500e998e881634cb64c01fea8bda490afd
Dylan Li <dylanli0629@gmail.com>
2022-01-22 02:49:31 -0500

    Initial Files

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+from flask import Flask, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+	
+if __name__ == '__main__':
+  app.run()
